ab4d252bdc075927d2e688de1fd53f2de0ab706a
ayubwishab <136605107+ayubwishab@users.noreply.github.com>
2026-05-03 08:34:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
-const CACHE_NAME = 'pro-scanner-v1';
+const CACHE_NAME = 'pro-scanner-v2';
 const ASSETS = [
-    './',
-    './index.html',
-    './app.js',
-    './icon-192.png',
+    './', './index.html', './app.js', './icon-192.png',
     'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
     'https://docs.opencv.org/4.5.4/opencv.js'
 ];
-
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
